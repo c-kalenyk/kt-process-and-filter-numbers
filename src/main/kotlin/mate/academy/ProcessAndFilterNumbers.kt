15f482adc3d9a03435b1124534606e149d9e88b3
Oleksandr Kalenyk <carbonmail88@gmail.com>
2025-08-22 14:40:37 +0300

    checkstyle fix

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,7 @@
 package mate.academy
 
+const val threshold = 25
+
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-    return numbers.map {if (it % 2 == 0) it / 2 else it * 2 }.filter { it > 25 }.toList()
+    return numbers.map {if (it % 2 == 0) it / 2 else it * 2 }.filter { it > threshold }.toList()
 }
